884e3657d2df6f689c78f6f34f8cc2fbe3e94b26
Lorenzo Carovillano <108892135+lorenzocarovillano@users.noreply.github.com>
2026-08-21 16:14:42 +0300

    feat(map): add light-dark Clay presentation and numbered markers

--- spike/flutter/lib/map_rendering.dart
+++ spike/flutter/lib/map_rendering.dart
@@ -5,6 +5,7 @@ class MapTheme {
   final double pitch;
 
   static const mintClay = MapTheme(routeColor: 0xFF00F5D4, backgroundColor: 0xFFF7F8FA, pitch: 48);
+  static const mintClayDark = MapTheme(routeColor: 0xFF2EC4B6, backgroundColor: 0xFF20252B, pitch: 48);
 }
 
 class RouteOverlay {
@@ -20,3 +21,17 @@ class MapMarker {
   final double longitude;
   final int color;
 }
+
+class MapPresentation {
+  const MapPresentation({required this.theme, required this.followPosition, required this.showOverview});
+  final MapTheme theme;
+  final bool followPosition;
+  final bool showOverview;
+  static const light = MapPresentation(theme: MapTheme.mintClay, followPosition: false, showOverview: true);
+  static const dark = MapPresentation(theme: MapTheme.mintClayDark, followPosition: false, showOverview: true);
+}
+
+List<MapMarker> numberedMarkers(List<MapMarker> markers) => [
+  for (var i = 0; i < markers.length; i++)
+    MapMarker(id: '\${i + 1}:\${markers[i].id}', latitude: markers[i].latitude, longitude: markers[i].longitude, color: markers[i].color),
+];
