d96ac985e939e1fe711097a08d19139e7cf8eff0
Lorenzo Carovillano <108892135+lorenzocarovillano@users.noreply.github.com>
2026-08-22 17:27:58 +0200

    show safe search failure diagnostics in Planner

--- spike/flutter/lib/planner.dart
+++ spike/flutter/lib/planner.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+import 'http_json_transport.dart';
 import 'route_models.dart';
 import 'route_orchestrator.dart';
 import 'routing_transport.dart';
@@ -199,7 +200,11 @@ class PlannerController extends ChangeNotifier {
       } catch (error) {
         if (id == _requestId) {
           suggestions = [];
-          this.error = 'Ricerca indirizzi non disponibile.';
+          this.error = error is JsonRequestException
+              ? 'Ricerca indirizzi non disponibile '
+                  '(HTTP ${error.statusCode}'
+                  '${error.code == null ? '' : ': ${error.code}'})'
+              : 'Ricerca indirizzi non disponibile.';
         }
       }
       if (id == _requestId) {
